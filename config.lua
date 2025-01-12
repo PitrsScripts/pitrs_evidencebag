2d753bebdef7303aff8acc45946c09a2826c28f7
Pitrs <150464632+PitrsScripts@users.noreply.github.com>
2025-01-12 01:21:18 +0100

    Delete config.lua

--- config.lua
+++
@@ -1,6 +0,0 @@
-Config = {}
-
-
-Config.Webhook = 'https://discord.com/api/webhooks/1327787103416877117/6vV0ZudsaGRUaFHJ65pBOuQrHE8MHBZzjLoTJkekcGRqoztx286NK9TeYNwSCiiJwAvo' -- Theres you set yours discord webhook
-
-
